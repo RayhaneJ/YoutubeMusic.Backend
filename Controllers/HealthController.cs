bba127eb56219357707efce031ef3e15f23ebe81
RayhaneJ <jebbarirayhane@gmail.com>
2025-12-01 00:02:47 +0100

    add health controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStreamServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult Health()
+        {
+            return Ok(new
+            {
+                status = "healthy",
+                timestamp = DateTime.UtcNow,
+                version = "1.0.0"
+            });
+        }
+    }
+}
